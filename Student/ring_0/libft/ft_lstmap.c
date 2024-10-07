93676d3d39a613fc0c59de539dc3aca9194a4261
Ayman Kamal Boulhand <akamal-b@c1r8s4.42madrid.com>
2024-10-07 19:25:45 +0200

    mini correcion

--- Student/ring_0/libft/ft_lstmap.c
+++ Student/ring_0/libft/ft_lstmap.c
@@ -6,13 +6,13 @@
 /*   By: akamal-b <akamal-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 13:58:05 by akamal-b          #+#    #+#             */
-/*   Updated: 2024/10/07 13:58:08 by akamal-b         ###   ########.fr       */
+/*   Updated: 2024/10/07 19:25:34 by akamal-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lstm void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lstm, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*lst_map;
 	t_list	*elem;
