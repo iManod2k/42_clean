e94a86c262d084fb463907172772c6bc949caf93
Ayman Kamal <contacto.ayman@gmail.com>
2024-10-17 19:14:31 +0200

    inestable pero completo

--- Student/ring_1/ft_printf.c
+++ Student/ring_1/ft_printf.c
@@ -6,20 +6,16 @@
 /*   By: akamal-b <akamal-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 00:49:48 by akamal-b          #+#    #+#             */
-/*   Updated: 2024/10/17 02:27:52 by akamal-b         ###   ########.fr       */
+/*   Updated: 2024/10/17 19:12:07 by akamal-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
-#include <stdarg.h>
-#include <limits.h>
-#include <stdio.h>
+#include "ft_printf.h"
 
 static void		print_prcnt_type(const char c,
 					const va_list args, const int fd);
 static void		digits(const char **s_aux,
 					const va_list args, const int fd);
-//static char		xtoa(size_t hex, const char letter_type);
 static void		xtoa_format(size_t hex,
 					const char letter_type, int format, const int fd);
 static void		ft_print_unsigned(unsigned int n, const int fd);
@@ -61,31 +57,16 @@ static void	digits(const char **s_aux, const va_list args, const int fd)
 	}
 	write(fd, ft_itoa(digit), ft_sizeint(digit));
 }
-/*
-static char	xtoa(size_t hex, const char letter_type)
-{
-	size_t	hex_aux;
-	char	letter;
 
-	letter = 'a';
-	hex_aux = 0;
-	if (letter_type == 'X')
-		letter = 'A';
-	hex_aux = hex % 16;
-	if (hex_aux > 0 && hex_aux <= 9)
-		return (hex_aux + '0');
-	else if (hex_aux >= 10 && hex_aux <= 16)
-		return ((hex_aux - 10) + letter);
-	return ('0');
-}
-*/
 static void	xtoa_format(size_t hex,
 					const char letter_type, int format, const int fd)
 {
 	char	*nums;
+	int		format_aux;
 	size_t	hex_aux;
 	size_t	hex_aux_mod;
 
+	format_aux = format;
 	hex_aux = hex;
 	nums = (char *)malloc((format + 1) * sizeof(char));
 	if (!nums)
@@ -94,9 +75,8 @@ static void	xtoa_format(size_t hex,
 		return ;
 	}
 	hex_aux = hex;
-	while (format > 0)
+	while (format-- > 0)
 	{
-		format--;
 		hex_aux_mod = (hex_aux % 16);
 		if (hex_aux_mod > 0 && hex_aux_mod <= 9)
 			nums[format] = hex_aux_mod + '0';
@@ -106,7 +86,7 @@ static void	xtoa_format(size_t hex,
 			nums[format] = '0';
 		hex_aux /= 16;
 	}
-	write(fd, nums, ft_strlen(nums));
+	write(fd, nums, format_aux);
 	free(nums);
 }
 
@@ -129,22 +109,25 @@ static void	print_prcnt_type(const char c, const va_list args, const int fd)
 	{
 		aux = ft_itoa(va_arg(args, int));
 		write(fd, aux, ft_strlen(aux));
+		free(aux);
 	}
 	else if (c == 'c')
 	{
-		c_aux = va_arg(args, int);
+		c_aux = (char)va_arg(args, int);
 		write(fd, &c_aux, 1);
 	}
 	else if (c == 's')
 	{
 		aux = va_arg(args, char *);
 		write(fd, aux, ft_strlen(aux));
+		free(aux);
+		//write(fd, va_arg(args, char *), ft_strlen(va_arg(args, char *)));
 	}
 	else if (c == 'p')
 	{
 		hex = (size_t) va_arg(args, void *);
 		write(fd, "0x", 2);
-		xtoa_format((size_t) hex, c, 12, fd);
+		xtoa_format((size_t) hex, 'x', 12, fd);
 	}
 	else if (c == 'x' || c == 'X')
 		xtoa_format((size_t) va_arg(args, int), c, 8, fd);
@@ -154,9 +137,11 @@ int	ft_printf(char const *frase, ...)
 {
 	va_list	args;
 	int		fd;
+	int		cont;
 
 	va_start(args, frase);
 	fd = STDOUT_FILENO;
+	cont = 0;
 	if (!frase)
 		return (0);
 	while (*frase)
@@ -169,33 +154,40 @@ int	ft_printf(char const *frase, ...)
 				digits(&frase, args, fd);
 			}
 			else
+			{
 				print_prcnt_type((*(++frase)), args, fd);
+				cont++;
+			}
 		}
 		else
 			write(fd, &*frase, 1);
 		frase++;
 	}
 	va_end(args);
-	return (0);
+	return (cont);
 }
 
+/*
+#include <stdio.h>
 int	main(void)
 {
 	
-	//char *frase = (char *)malloc((4 + 1)* sizeof(char));
-	
-	//ft_strlcpy(frase, "Hola", 4);
-	//("%p\n", &frase);
-	//ft_("%p\n", &frase);
-	
+	// char *frase = (char *)malloc((4 + 1)* sizeof(char));
+	// ft_strlcpy(frase, "Hola", 4);
+	// printf("%p\n", &frase);
+	// ft_printf("%p\n", &frase);
 
+	// char *frase = (char *)malloc((4 + 1)* sizeof(char));
+	// ft_strlcpy(frase, "Hola", 5);
+	// printf("%s\n", frase);
+	// ft_printf("%s\n", frase);
 	
-	int number = -14423;
-	printf("printf: %x - %X\n", (int)number, (int)number);
-	ft_printf("ft_printf: %x - %X\n", (int)number, (int)number);
+	// int number = -14423;
+	// printf("printf: %x - %X\n", (int)number, (int)number);
+	// ft_printf("ft_printf: %x - %X\n", (int)number, (int)number);
 	
-	//unsigned int number = 3147483648;
-	//ft_printf("ft_print: %u\n", number);
+	ft_printf(" %c ", '0');
 
 	return (0);
 }
+*/
---
+++ Student/ring_1/ft_printf.h
@@ -0,0 +1,10 @@
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+# include <limits.h>
+
+int     ft_printf(char const *, ...);
+
+#endif
