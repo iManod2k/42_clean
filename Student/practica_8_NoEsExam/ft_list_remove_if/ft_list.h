332e312985a5a3821829201736bb32d53376a226
akamal-b <akamal-b@Ubuntu42.myguest.virtualbox.org>
2025-01-25 23:21:07 +0100

    subo

--- Student/practica_8_NoEsExam/ft_list_remove_if/ft_list.h
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-typedef struct      s_list
-{
-    struct s_list   *next;
-    void            *data;
-}                   t_list;
-
-void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)());
